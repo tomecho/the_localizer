b1a9fdf3e68f595f32710b0cc45a3c9c4561593e
Tom Peck <tom@tompeck.me>
2023-09-27 23:18:04 -0400

    update the test site with a button that adds content on click

--- testsite/index.js
+++ testsite/index.js
@@ -1,18 +1,26 @@
 // test adding some dynamic content
 
+function writeElement(text, blockTranslation) {
+    const newNode = document.createElement('p');
+    if (blockTranslation) {
+        newNode.setAttribute('data-no-translate', 'true');
+    }
+    newNode.innerHTML = text;
+    document.body.appendChild(newNode);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        const newNode = document.createElement('p');
-        newNode.innerHTML = 'Hello this is a dynamicly created element that should be translated';
-        document.body.appendChild(newNode);
+        writeElement('Hello this is a dynamicly created element that should be translated');
     }, 300);
 
     setTimeout(() => {
-        const newNode = document.createElement('p');
-        newNode.setAttribute('data-no-translate', 'true');
-        newNode.innerHTML = 'Hello this is a dynamicly created element that should not be translated';
-        document.body.appendChild(newNode);
+        writeElement('Hello this is a dynamicly created element that should not be translated', true);
     }, 300);
+});
+
+document.querySelector('button').addEventListener('click', () => {
+    writeElement('Hello this content was created by a button and should be translated');
 });
 
 InjectLocalizer({ supportLangs: ["es", "he"], fromLang: "en" });
